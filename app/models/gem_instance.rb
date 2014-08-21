9beecb52c47b95adea7413957ad40f65530334ff
Khadija Wharton <khadija@kw.local>
2014-08-21 15:52:27 -0400

    most popular version method on gem

--- app/models/gem_instance.rb
+++ app/models/gem_instance.rb
@@ -4,5 +4,11 @@ class GemInstance < ActiveRecord::Base
   def num_gem_uses
     GemUse.where(gem_instance: self).count
   end
+
+  def most_popular_version
+    versions = GemUse.where(gem_instance: self).group(:version).count
+    return versions.max_by{|k, v| v}[0]
+  end
+
 end
 
