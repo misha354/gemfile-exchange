cb84e9368b8e3e64c98872a18021444304335e09
Khadija Wharton <khadija@kw.local>
2014-08-21 23:14:46 -0400

    my wife needs a massage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,23 @@
 class SessionsController < ApplicationController
 
-def create
-end
+  def create
+    user = User.find_by username: params[:username]
 
+    if user && user.authenticate(params[:password])
+      login_user!(user) 
+      redirect_to root_path
+    else
+      flash[:error] = "There is something wrong with your username "
+      redirect_to login_path
+    end
+  end
+
+  def login_user!(user)
+    session[:user_id] = user.id
+    flash[:notice] = 'You have succesfully logged in.'
+  end
+
+  def destroy
+    session[:id] = nil
+  end
 end
