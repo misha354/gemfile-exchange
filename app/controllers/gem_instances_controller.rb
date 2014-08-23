c6a01abc6437277607bc70b1fbaa1698a9d577e3
Khadija Wharton <khadija@kw.local>
2014-08-22 22:39:35 -0400

    Change gem show to use gemfile partial and fix gemfile

--- app/controllers/gem_instances_controller.rb
+++ app/controllers/gem_instances_controller.rb
@@ -6,5 +6,6 @@ class GemInstancesController < ApplicationController
   def show
     @gem_instance = GemInstance.find(params[:id])
     @gem_info = Gems.info(@gem_instance.name)
+    @gemfiles = @gem_instance.gemfiles.sort_by {|x| x.total_votes}.reverse
   end
 end
