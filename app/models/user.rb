f7f65116622bd261aeae3ac68aaca6d5d2bc8b21
Khadija Wharton <khadija@kw.local>
2014-08-22 21:23:43 -0400

    added user and time to gemfile view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password validations: false
+
+  has_many :gemfiles
+
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: {minimum: 5} 
 end
