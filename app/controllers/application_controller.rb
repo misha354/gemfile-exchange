3b8ea1ea1c6e1ded90dc5ff5d080d496a639b2d9
Khadija Wharton <khadija@kw.local>
2014-08-22 22:13:39 -0400

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   def require_user
     if !logged_in?
       flash[:error] = "You must be logged in to do that."
-      redirect_to :back
+      redirect_to root_path
     end
   end
 end
